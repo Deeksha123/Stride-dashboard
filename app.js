58b9f9ed40ccac1546ab66ad9a28902215a97bec
Diksha Awasthi <dawasthi@xavient.com>
2018-10-25 16:31:55 +0530

    Dashboard app created.

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var routes = require("./routes/routes.js");
+var app = express();
+
+//The below two lines tells express to accept both JSON and url encoded values
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+routes(app);
+
+var server = app.listen(3000, function () {
+    console.log("app running on port.", server.address().port);
+});
---
+++ routes/routes.js
@@ -0,0 +1,9 @@
+var appRouter = function (app) {
+
+    app.get("/", ( req, res ) => {
+        res.send("Hello World");
+    });
+
+}
+  
+module.exports = appRouter;
